350de30b1299b2a21d3c78abaad97e70edf1d161
adam <amartenj@gmail.com>
2017-07-18 14:21:37 -0700

    Splash page styling

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -3,10 +3,21 @@ import { Link } from 'react-router-dom';
 
 const Splash = () => {
   return (
-    <div>
-      Where Work Happens
+    <div className="splash-page">
 
-      <Link to="/login">Get Started</Link>
+      <div className="splash-content">
+        <div className="splash-text">
+          <div>Where work<span className="asterisk">*</span></div>
+          <div>happens</div>
+        </div>
+        <div className="splash-tagline">
+          <span className="asterisk">*</span>Whatever work means for you, Tension brings all the pieces and people
+          you need together so you can actually get things done.
+        </div>
+
+        <Link className="button" to="/login">Get Started</Link>
+      </div>
+
     </div>
   );
 };
