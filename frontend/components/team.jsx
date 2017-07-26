41f543da778dc86874a7d593dc5c40fbead0438e
adam <amartenj@gmail.com>
2017-07-26 11:29:50 -0700

    Added dropdown menu

--- frontend/components/team.jsx
+++ frontend/components/team.jsx
@@ -17,19 +17,59 @@ class Team extends React.Component {
     }
   }
 
+  showDropdown() {
+    $('#team-dropdown').addClass('show');
+  }
+
+  hideDropdown() {
+    $('#team-dropdown').removeClass('show');
+  }
+
   render() {
     return(
       <div className="team-container">
         <div className="team-content">
 
-          <h3 className="team-name">{this.props.team.name}</h3>
-
-          <div className="user-status">
-            <i className="fa fa-circle available" aria-hidden="true"></i>
-            <span className="username">{this.props.currentUser.username}</span>
+          <div className="clickable" onClick={this.showDropdown}>
+            <div className="side-by-side">
+              <h3 className="team-name">{this.props.team.name}</h3>
+              &nbsp;
+              <i className="fa fa-caret-down" aria-hidden="true"></i>
+            </div>
+            <div className="user-status">
+              <i className="fa fa-circle available" aria-hidden="true"></i>
+              <span className="username">{this.props.currentUser.username}</span>
+            </div>
           </div>
 
-          <button onClick={this.props.logout}>Logout</button>
+          <div id="team-dropdown" className="dropdown">
+
+            <div className="dropdown-content">
+              <div className="dropdown-section">
+                <div className="side-by-side">
+                  <img className="avatar" src="http://res.cloudinary.com/dwczmcdof/image/upload/v1500674716/avatar-generic-m.png"/>
+                  <span className="username">{this.props.currentUser.username}</span>
+                </div>
+              </div>
+
+              <div className="divider"></div>
+
+              <div className="dropdown-section">
+                <div className="dropdown-item" onClick={this.props.logout}>Logout</div>
+                <div className="dropdown-item"><Link className="fill" to="/teams">Change Team</Link></div>
+              </div>
+
+              <div className="divider"></div>
+
+              <div className="dropdown-section">
+                <div className="dropdown-item"><a className="fill" target="_blank" href="https://github.com/AdamJacobson/Tension-Chat">About Tension</a></div>
+              </div>
+
+            </div>
+
+            <div className="dropdown-mask" onClick={this.hideDropdown}></div>
+          </div>
+
 
           <ChannelsContainer />
         </div>
